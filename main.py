a06ffb9b3492a3c8c1b6cdeb8dba73cf94e6325a
tristinfriend <tristin.friend@maine.edu>
2024-11-07 11:50:39 -0500

    Update #4

--- main.py
+++ main.py
@@ -8,14 +8,27 @@ def createBoard(boardSize):
     return board
             
 def printBoard(board):
+    outputString = "" #empty string
     for row in board: 
-
+        
 
         for col in row:
-            print(f"[{col:2}]", end="")
-        print() 
+            outputString += f"[{col:2}]"
+        outputString += "\n"
+    print(outputString, end ="")
 
+def getUserDir():
+    
+    validCommands = ['n','e','w','s']
 
+    while True:
+        userInput = input("Enter a direction (n,e,w,s): ")
+        userInput = userInput.lower().strip()
+        if userInput not in validCommands: 
+            print("Not a valid command")
+            continue 
+        return userInput
 
 b = createBoard(4)
 printBoard(b)
+u = getUserDir()
