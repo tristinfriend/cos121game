dba8800367d643f1cd8f9a56bf5500e3bae0e9ed
tristinfriend <tristin.friend@maine.edu>
2024-11-07 11:29:59 -0500

    Update #2

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 
-boardSize = 4
-board = []
-for row in range(boardSize): 
-    board.append([]) 
-    for col in range(boardSize):
-        board[row].append(row * boardSize + col)
-        
+def createBoard(boardSize):
+    board = []
+    for row in range(boardSize): 
+        board.append([]) 
+        for col in range(boardSize):
+            board[row].append(row * boardSize + col)
+    return board
+            
+def printBoard(board):
+    for row in board: 
 
-print(board) 
+
+        for col in row:
+            print(f"[{col:2}]", end="")
+        print() 
+
+
+
+b = createBoard(4)
+printBoard(b)
